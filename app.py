5afa03d881e52ecc122765fc4a7e37068932499b
Bratee Podder <bpodder3@gatech.edu>
2026-04-20 19:15:08 -0400

    small update to layout

--- app.py
+++ app.py
@@ -4,6 +4,32 @@ import pandas as pd
 st.title("StressLens")
 st.write("A stress detection and monitoring system for cardiac rehabilitation patients.")
 
+st.header("Upload Your Data")
+st.write("Here is where you can upload your data associated from your Empatica E4, or any other device that measures these signals.")
+ACC = st.file_uploader("Upload the accelerometer data from Empatica E4.")
+if ACC is not None:
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(ACC)
+    st.write(dataframe)
+
+BVP = st.file_uploader("Upload the BVP data from Empatica E4.")
+if BVP is not None:
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(BVP)
+    st.write(dataframe)
+
+EDA = st.file_uploader("Upload the EDA data from Empatica E4.")
+if EDA is not None:
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(EDA)
+    st.write(dataframe)
+    
+TEMP = st.file_uploader("Upload the temperature data from Empatica E4.")
+if TEMP is not None:
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(TEMP)
+    st.write(dataframe)
+
 with st.sidebar:
     with st.container():
         st.header("What is StressLens?")
@@ -12,28 +38,4 @@ with st.sidebar:
         "inspired by current stress-logs, such as University of Ottowa Heart Institute's Stress Management guide. This allows for caregivers to track patient's" \
         "progress on using certain relaxation techniques, which triggers are most prominent and require intervention, and signal if the patient has low HRV " \
         "indices tied to higher mortality rates.")
-
-    ACC = st.file_uploader("Upload the accelerometer data from Empatica E4.")
-    if ACC is not None:
-        # Can be used wherever a "file-like" object is accepted:
-        dataframe = pd.read_csv(ACC)
-        st.write(dataframe)
-
-    BVP = st.file_uploader("Upload the BVP data from Empatica E4.")
-    if BVP is not None:
-        # Can be used wherever a "file-like" object is accepted:
-        dataframe = pd.read_csv(BVP)
-        st.write(dataframe)
-
-    EDA = st.file_uploader("Upload the EDA data from Empatica E4.")
-    if EDA is not None:
-        # Can be used wherever a "file-like" object is accepted:
-        dataframe = pd.read_csv(EDA)
-        st.write(dataframe)
-    
-    TEMP = st.file_uploader("Upload the temperature data from Empatica E4.")
-    if TEMP is not None:
-        # Can be used wherever a "file-like" object is accepted:
-        dataframe = pd.read_csv(TEMP)
-        st.write(dataframe)
 
